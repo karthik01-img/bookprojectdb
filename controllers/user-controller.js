b12d4a99b4ef343f5463474e643159beaba5a728
karthik01-img <kkorukonda1@gmail.com>
2025-12-22 16:15:29 +0530

    22/12

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,6 @@
 var user = require("../model/user")
 var bcrypt = require("bcrypt")
+var jwt = require("jsonwebtoken")
 
 
 var createnewuser = async(req,res)=>{
@@ -7,7 +8,7 @@ var createnewuser = async(req,res)=>{
         var {username,email,password,role} = req.body
         var existuser = await user.findOne({$or :[{username,email}]})
         if(existuser){
-            res.status(200).json({message: "user already exists"})
+         return   res.status(200).json({message: "user already exists"})
         }
         var salt = await bcrypt.genSalt(10)
         var hashedpass = await bcrypt.hash(password,salt)
@@ -18,12 +19,37 @@ var createnewuser = async(req,res)=>{
             password : hashedpass,
             role
         })
-        res.status(200).json({msg : "user created successfully"})
+       return res.status(200).json({msg : "user created successfully"})
     }
     catch(error){
         console.log("error",error);
-        res.status(500).json({message: "error occured"})
+      return  res.status(500).json({message: "error occured"})
     }
 }
 
-module.exports = createnewuser
+var loginuser = async(req,res)=>{
+    try{
+var {username,password}= req.body
+var checkuser = await user.findOne({username})
+if(!checkuser){
+    return res.status(404).json({message : "invalid creditintials"})
+}
+var checkpassword = await bcrypt.compare(password,checkuser.password)
+if(!checkpassword){
+    return res.status(404).json({message : "invalid credintials"})
+}
+
+var token = jwt.sign({
+    userid : checkuser._id,
+    username : checkuser.username,
+    role : checkuser.role
+},process.env.JWT_TOKEN,{expiresIn : "2h"})
+
+res.status(200).json({messgae :"login successfull", mytoken : token})
+    }
+    catch(error){
+        console.log("error",error);
+    }
+}
+
+module.exports = {createnewuser,loginuser}
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,11 @@
 var express = require("express")
-const createnewuser = require("../controllers/user-controller")
+const {createnewuser,loginuser} = require("../controllers/user-controller")
 
 var router = express.Router()
 
 
 router.post("/register",createnewuser)
+
+router.post("/login",loginuser)
 
 module.exports = router
